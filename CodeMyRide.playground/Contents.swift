fdddb10b997c1b38d699220b08b59fbacd00f23d
Rishi Masand <rishimasand@Rishis-iMac.local>
2017-03-30 13:09:10 -0400

    added control panel with buttons

--- CodeMyRide.playground/Contents.swift
+++ CodeMyRide.playground/Contents.swift
@@ -5,11 +5,27 @@ let car = Car()
 
 PlaygroundPage.current.liveView = car
 
-_ = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+_ = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
     if (car.acceleratorPedalSpriteTapped){
         car.speedUp()
     }
     if (car.brakePedalSpriteTapped){
-        car.slowDown()
+        car.stop()
+        car.turnOnBrakelight()
+    }
+    else {
+        car.turnOffBrakelight()
+    }
+    if (car.isRaining){
+        car.turnOnWiper()
+    }
+    else if (car.isWiping) {
+        car.turnOffWiper()
+    }
+    if (car.isDay){
+        car.turnOffHeadlight()
+    }
+    else {
+        car.turnOnHeadlight()
     }
 }
--- CodeMyRide.playground/Sources/car.swift
+++ CodeMyRide.playground/Sources/car.swift
@@ -15,8 +15,20 @@ public class Car: SKView {
     
     public var acceleratorPedalSpriteTapped: Bool = false
     public var brakePedalSpriteTapped: Bool = false
+    public var rainButtonTapped: Bool = false
+    public var dayNightButtonTapped: Bool = false
     
-    var rainParticle: SKEmitterNode = SKEmitterNode()
+    public var isInMotion: Bool = false
+    public var isRaining: Bool = false
+    public var isWiping: Bool = false
+    public var isDay: Bool = true
+    
+    public var rainButton: SKSpriteNode = SKSpriteNode()
+    public var dayNightButton: SKSpriteNode = SKSpriteNode()
+    
+    public var rainParticle: SKEmitterNode = SKEmitterNode()
+    
+    public var controlPanel: SKSpriteNode = SKSpriteNode()
     
     public init(){
         
@@ -67,26 +79,49 @@ public class Car: SKView {
         wiperSprite.size = CGSize(width: 50, height: 50)
         scene.addChild(wiperSprite)
         
+        controlPanel.size = CGSize(width: scene.frame.size.width, height: 60)
+        controlPanel.position = CGPoint(x: scene.frame.size.width / 2, y: 30)
+        controlPanel.color = UIColor.gray
+        scene.addChild(controlPanel)
+        
         acceleratorPedalSprite = SKSpriteNode(imageNamed: "pedal")
         acceleratorPedalSprite.size = CGSize(width: 30, height: 60)
-        acceleratorPedalSprite.position = CGPoint(x: 480, y: 260)
+        acceleratorPedalSprite.position = CGPoint(x: 480, y: 30)
         scene.addChild(acceleratorPedalSprite)
         
         brakePedalSprite = SKSpriteNode(imageNamed: "pedal")
         brakePedalSprite.size = CGSize(width: 30, height: 60)
-        brakePedalSprite.position = CGPoint(x: 32, y: 260)
+        brakePedalSprite.position = CGPoint(x: 440, y: 30)
         scene.addChild(brakePedalSprite)
         
         headlightSprite.isHidden = true
         brakelightSprite.isHidden = true
         
+        rainButton.size = CGSize(width: 50, height: 50)
+        rainButton.position = CGPoint(x: 50, y: 30)
+        rainButton.color = UIColor.blue
+        scene.addChild(rainButton)
+        
+        dayNightButton.size = CGSize(width: 50, height: 50)
+        dayNightButton.position = CGPoint(x: 120, y: 30)
+        dayNightButton.color = UIColor.black
+        scene.addChild(dayNightButton)
+        
         presentScene(scene)
+        
+        _ = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            
+            
+            
+        }
         
     }
     
     public func speedUp(){
-        wheelBackSprite.run(SKAction.repeatForever((SKAction.rotate(byAngle: (-2) * .pi, duration: 2))))
-        wheelFrontSprite.run(SKAction.repeatForever((SKAction.rotate(byAngle: (-2) * .pi, duration: 2))))
+        if (!isInMotion){
+            isInMotion = true
+            wheelBackSprite.run(SKAction.repeatForever((SKAction.rotate(byAngle: (-2) * .pi, duration: 2))), withKey: "rotate")
+        wheelFrontSprite.run(SKAction.repeatForever((SKAction.rotate(byAngle: (-2) * .pi, duration: 2))), withKey: "rotate")
         backgroundSprite0.run(SKAction.repeatForever((SKAction.sequence([SKAction.moveBy(x: -512, y: 0, duration: 3), SKAction.move(to: CGPoint(x: 256, y: 256), duration: 0)]))), withKey: "scroll")
         backgroundSprite1.run(SKAction.repeatForever((SKAction.sequence([SKAction.moveBy(x: -512, y: 0, duration: 3), SKAction.move(to: CGPoint(x: 256 + 512, y: 256), duration: 0)]))), withKey: "scroll")
         carBodySprite.run(SKAction.repeatForever(SKAction.sequence([SKAction.moveBy(x: 0, y: 3, duration: 0.24), SKAction.moveBy(x: 0, y: -3, duration: 0.24)])), withKey: "bounce")
@@ -95,6 +130,7 @@ public class Car: SKView {
         headlightSprite.run(SKAction.repeatForever(SKAction.sequence([SKAction.moveBy(x: 0, y: 3, duration: 0.24), SKAction.moveBy(x: 0, y: -3, duration: 0.24)])), withKey: "bounce")
         brakelightSprite.run(SKAction.repeatForever(SKAction.sequence([SKAction.moveBy(x: 0, y: 3, duration: 0.24), SKAction.moveBy(x: 0, y: -3, duration: 0.24)])), withKey: "bounce")
         wiperSprite.run(SKAction.repeatForever(SKAction.sequence([SKAction.moveBy(x: 0, y: 3, duration: 0.24), SKAction.moveBy(x: 0, y: -3, duration: 0.24)])), withKey: "bounce")
+        }
     }
     
     public func slowDown(){
@@ -107,8 +143,9 @@ public class Car: SKView {
     }
     
     public func stop(){
-        wheelBackSprite.run(SKAction.repeatForever((SKAction.rotate(byAngle: (0) * .pi, duration: 2))))
-        wheelFrontSprite.run(SKAction.repeatForever((SKAction.rotate(byAngle: (0) * .pi, duration: 2))))
+        isInMotion = false
+        wheelBackSprite.removeAction(forKey: "rotate")
+        wheelFrontSprite.removeAction(forKey: "rotate")
         backgroundSprite0.removeAction(forKey: "scroll")
         backgroundSprite1.removeAction(forKey: "scroll")
         carBodySprite.removeAction(forKey: "bounce")
@@ -136,11 +173,19 @@ public class Car: SKView {
     }
     
     public func turnOnWiper(){
-        wiperSprite.run(SKAction.repeatForever(SKAction.sequence([SKAction.rotate(byAngle: 0.08 * .pi, duration: 1), SKAction.rotate(byAngle: -0.08 * .pi, duration: 1)])), withKey: "wipe")
+        print("on")
+        if (!isWiping){
+            print("actually on")
+            isWiping = true
+            wiperSprite.run(SKAction.repeatForever(SKAction.sequence([SKAction.rotate(byAngle: 0.08 * .pi, duration: 1), SKAction.rotate(byAngle: -0.08 * .pi, duration: 1)])), withKey: "wipe")
+        }
     }
     
     public func turnOffWiper(){
+        print("off")
         wiperSprite.removeAction(forKey: "wipe")
+        isWiping = false
+        wiperSprite.run(SKAction.rotate(toAngle: 0, duration: 1))
     }
     
     public func startRaining(){
@@ -157,14 +202,20 @@ public class Car: SKView {
         self.scene?.addChild(rainParticle)
     }
     
+    public func stopRaining(){
+        rainParticle.removeFromParent()
+    }
+    
     public func becomeNight(){
         backgroundSprite0.texture = SKTexture(imageNamed: "background_night")
         backgroundSprite1.texture = SKTexture(imageNamed: "background_night")
+        dayNightButton.color = UIColor.yellow
     }
     
     public func becomeDay(){
         backgroundSprite0.texture = SKTexture(imageNamed: "background")
         backgroundSprite1.texture = SKTexture(imageNamed: "background")
+        dayNightButton.color = UIColor.black
     }
     
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -176,12 +227,38 @@ public class Car: SKView {
             print("acceleratorPedalSprite tapped")
             acceleratorPedalSpriteTapped = true
         }
+        else if (abs((touches.first?.location(in: rainButton).x)!) <= (rainButton.size.width / 2) && abs((touches.first?.location(in: rainButton).y)!) <= (rainButton.size.height / 2)){
+            print("rainButton tapped")
+            rainButtonTapped = true
+            if (!isRaining){
+                startRaining()
+                isRaining = true
+            }
+            else {
+                stopRaining()
+                isRaining = false
+            }
+        }
+        else if (abs((touches.first?.location(in: dayNightButton).x)!) <= (dayNightButton.size.width / 2) && abs((touches.first?.location(in: dayNightButton).y)!) <= (dayNightButton.size.height / 2)){
+            print("dayNightButton tapped")
+            dayNightButtonTapped = true
+            if (isDay){
+                becomeNight()
+                isDay = false
+            }
+            else {
+                becomeDay()
+                isDay = true
+            }
+        }
     }
     
     public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("touches ended")
         brakePedalSpriteTapped = false
         acceleratorPedalSpriteTapped = false
+        rainButtonTapped = false
+        dayNightButtonTapped = false
     }
     
     required public init?(coder aDecoder: NSCoder) {
